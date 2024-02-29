67a69ea4164823e716da1df9f55517b9a046da4d
vidyamarathi <vidya.marathi.4work@mail.com>
2024-02-29 21:12:59 +0530

    modify file

--- axiosreact/src/main.jsx
+++ axiosreact/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import Data_table from './axrequest/Data_table'
+//import Data_table from './axrequest/Data_table'
+import Bootstrap from './axrequest/Bootstrap'
+
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Data_table/>
+    {/* <Data_table/> */}
+    <Bootstrap/>
   </React.StrictMode>,
 )
